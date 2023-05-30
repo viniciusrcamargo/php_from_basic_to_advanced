e51b1cbe89665a02e3f209c21608886973d36b60
Vinicius Rodrigues Camargo <viniciusr.camargo@gmail.com>
2023-05-29 21:13:00 -0300

    Studying OOP in PHP

---
+++ oop/src/conta.php
@@ -0,0 +1,13 @@
+<?php
+
+
+function criarConta(string $cpf, string $nomeTitular, float $saldo): array
+{
+    return [
+        $cpf => [
+            'titular' => $nomeTitular,
+            'saldo' => $saldo,
+        ]
+    ];
+
+}
